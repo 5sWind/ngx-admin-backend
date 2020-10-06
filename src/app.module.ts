3ce9b99302fc8f3a9cf3339b4feb6bfe45091651
Hengming Zhang <fiveswind@gmail.com>
2020-10-07 00:29:44 +0800

    Scripted auto-commit on change (2020-10-07 00:29:44) by gitwatch.sh

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,17 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [AuthModule, UsersModule,
-  ],
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
