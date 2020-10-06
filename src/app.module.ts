5040b18caff1029617bd26bdcbea850c83490870
Hengming Zhang <fiveswind@gmail.com>
2020-10-07 00:35:24 +0800

    Scripted auto-commit on change (2020-10-07 00:35:24) by gitwatch.sh

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 3306,
+      port: 5432,
       username: 'root',
       password: 'root',
       database: 'test',
