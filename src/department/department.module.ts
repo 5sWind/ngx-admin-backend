0feb04de1dbd27bc4ccec1b4e139ffbb005f38bd
Hengming Zhang <fiveswind@gmail.com>
2020-10-07 15:42:10 +0800

    Scripted auto-commit on change (2020-10-07 15:42:10) by gitwatch.sh

--- src/department/department.module.ts
+++ src/department/department.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { DepartmentService } from './department.service';
 
-@Module({})
+@Module({
+  providers: [DepartmentService]
+})
 export class DepartmentModule {}
